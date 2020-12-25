b2e5790165c54a094425de94b6a9925ec6d788fd
xiaoyiluo233 <2690291311@qq.com>
2020-12-25 09:47:52 +0800

    第一次项目搭建

---
+++ src/main/java/com/yidu/AnnengApplication.java
@@ -0,0 +1,13 @@
+package com.yidu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnengApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnnengApplication.class, args);
+    }
+
+}
